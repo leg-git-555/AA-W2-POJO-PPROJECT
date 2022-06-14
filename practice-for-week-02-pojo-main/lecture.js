895671978ff9d56d71d7ed007c7937663f81788e
leg-git-555 <103963029+leg-git-555@users.noreply.github.com>
2022-06-14 18:18:34 -0400

    Add files via upload
    uploaded from my machine

---
+++ practice-for-week-02-pojo-main/lecture.js
@@ -0,0 +1,53 @@
+// // primitive vs reference types
+
+// // let myArr = ["this"]
+// let age = 20
+// let age2 = age
+// age ++
+// //console.log(age, age2)
+
+// let myArr = ['this']
+// let myArr2 = myArr.slice()
+// myArr.push('that')
+// //console.log(myArr, myArr2)
+// myArr.push(age)
+// //console.log(myArr)
+
+// let myObj = {} // instantiating an object
+// myObj['cat'] = 'count chonkula' // adding key cat with value 'count chonkula'
+// myObj.otherCat = 'wax' //adding ket otherCat with value 'wax'
+// //console.log(myObj)// logging object to console
+
+
+// let series = {
+//     name: "Stormlight Archives",
+//     books : {
+//         first: 'the way of kings',
+//         second: 'words of radiance',
+//         third: 'oathbreaker',
+//         fourth: 'rhythm of war',
+//     },
+//     author: "Brandon Sanderson",
+//     numberOfNovellas: 1,
+//     worthReading: false,
+//     quality: ['third', 'first', 'fourth', 'second']
+// }
+
+// for (let key in series) {
+    // console.log("Current key:", key)
+//     console.log("            it's value is:", series[key])
+// }
+
+// let values = Object.values(series)
+// console.log(values)
+
+
+// let myArr = []
+
+// let myPusher = (things, things2, things3) => {
+//     console.log(things, things2, things3)
+// }
+
+// myPusher(...[1,2,3,4,5,6,7,8])
+
+
